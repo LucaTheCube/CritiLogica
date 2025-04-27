d8f1347a3a043c7d5d2cc3ead699086893b2c2ae
LucaTheCube <nonnoiluca@gmail.com>
2025-04-27 10:32:12 +0200

    Delete sito logica directory

--- sito logica/script.js
+++
@@ -1,8 +0,0 @@
-function mostraCommento() {
-    document.getElementById('commento').style.display = 'inline';
-  
-  }
-  
-  function nascondiCommento() {
-    document.getElementById('commento').style.display = 'none';
-  }
