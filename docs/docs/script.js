ceef93c2444a110ed97b6faf161fdd239aebaccb
LucaTheCube <nonnoiluca@gmail.com>
2025-07-14 19:02:10 +0200

    Delete docs/docs/script.js

--- docs/docs/script.js
+++
@@ -1,8 +0,0 @@
-function mostraCommento() {
-    document.getElementById('commento').style.display = 'block';
-  
-  }
-  
-  function nascondiCommento() {
-    document.getElementById('commento').style.display = 'none';
-  }
